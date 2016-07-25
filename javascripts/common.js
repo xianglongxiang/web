50d9a2b5f5ffd3c1144c73372e658df642802b2e
xianglongxiang <xlx_good@163.com>
2016-07-25 10:34:11 +0800

    add random

--- javascripts/common.js
+++ javascripts/common.js
@@ -42,8 +42,7 @@
      *  @return [string]
      * */
     _global.getFileUrl = function(file){
-        if(file.files&&file.files[0])
-            return window.URL.createObjectURL(file.files[0]);
+        if(file.files&&file.files[0])return window.URL.createObjectURL(file.files[0]);
         file.select();
         return doc.selection.createRange().text;
     }
@@ -54,6 +53,60 @@
     _global.getFileSize = function(file){
         return Math.ceil(file.files[0].size/1024);
     }
+
+
+    /**
+     *  事件绑定
+     *  @param element 需要绑定的对象 eg：dom、window
+     *  @param type 绑定的时间类型  eg：click、dbclick
+     *  @param handler 回调方法
+     * */
+    _global.addEvent = function(element, type, handler) {
+        if(element.addEventListener) {
+            addEvent = function(element, type, handler) {
+                element.addEventListener(type, handler, false);
+            };
+        } else if(element.attachEvent) {
+            addEvent = function(element, type, handler) {
+                element.attachEvent('on' + type, handler);
+            };
+        } else {
+            addEvent = function(element, type, handler) {
+                element['on' + type] = handler;
+            };
+        }
+    };
+
+    /**
+     *  获取数组最小值
+     *  @param arr  数组
+     *  @return 数组中的最小值
+     * */
+    _global.getMinVal = function(arr) {
+        return Math.min.apply(Math, arr);
+    };
+    /**
+     * 获取随机码
+     * @param num  获取随机码的位数
+     * @return 返回随机码
+     * */
+    _global.getAutoAesKey = function(num){
+        var randomStr = "";
+        var strArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+            'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z'];
+        for(var i = 0; i< num; i++){
+            var index = Math.round(Math.random()*42);
+            randomStr += strArray[index];
+        }
+        return randomStr;
+    },
+    win.global  = _global.lx = _global;
+})(window, document);
+
+void function(win,doc){
+    var _global = {};
     /**
      * 保存文件，将文件下载到本地
      * @param value 保存的内容
@@ -74,16 +127,16 @@
         }
         var URL = window.URL || window.webkitURL;
         var bloburl = URL.createObjectURL(blob);
-        var anchor = document.createElement("a");
+        var anchor = doc.createElement("a");
         if ('download' in anchor) {
             anchor.style.visibility = "hidden";
             anchor.href = bloburl;
             anchor.download = name;
-            document.body.appendChild(anchor);
-            var evt = document.createEvent("MouseEvents");
+            doc.body.appendChild(anchor);
+            var evt = doc.createEvent("MouseEvents");
             evt.initEvent("click", true, true);
             anchor.dispatchEvent(evt);
-            document.body.removeChild(anchor);
+            doc.body.removeChild(anchor);
         } else if (navigator.msSaveBlob) {
             navigator.msSaveBlob(blob, name);
         } else {
@@ -91,39 +144,6 @@
         }
     }
     win.global  = _global.lx = _global;
-})(window, document);
-void function(){
-    /**
-     *  事件绑定
-     *  @param element 需要绑定的对象 eg：dom、window
-     *  @param type 绑定的时间类型  eg：click、dbclick
-     *  @param handler 回调方法
-     * */
-    var addEvent = function(element, type, handler) {
-        if(element.addEventListener) {
-            addEvent = function(element, type, handler) {
-                element.addEventListener(type, handler, false);
-            };
-        } else if(element.attachEvent) {
-            addEvent = function(element, type, handler) {
-                element.attachEvent('on' + type, handler);
-            };
-        } else {
-            addEvent = function(element, type, handler) {
-                element['on' + type] = handler;
-            };
-        }
-        addEvent(element, type, handler);
-    };
-
-    /**
-     *  获取数组最小值
-     *  @param arr  数组
-     *  @return 数组中的最小值
-     * */
-    var getMinVal = function(arr) {
-        return Math.min.apply(Math, arr);
-    };
-}(window,document,undefined)
+}(window,document)
 
 
